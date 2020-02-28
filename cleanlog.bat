0a90c0ccc2a3b4980c7c857bd895f191e2298ee7
John Vigilante <jvigilan@gmail.com>
2020-02-28 15:24:58 -0600

    Remove cleanlog.bat from vcs.

--- cleanlog.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd C:\Users\John\PycharmProjects\Reboot_Router
-for /f %%a in ('powershell -Command "Get-Date -format yyyy_MM_dd__HH_mm_ss"') do set datetime=%%a
-copy routercheck.log %datetime%_Backup_routercheck.log
-break>routercheck.log
-copy geckodriver.log %datetime%_Backup_geckodriver.log
-break>geckodriver.log
-rem delete older logs - keep last 4 weeks
-for /f "skip=8 eol=: delims=" %%F in ('dir /b /o-d /a-d 2*.log') do @del "%%F"
